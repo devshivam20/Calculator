164cfb1eda23dd45856a489d2a44fbc91ddb24cc
ShivamG-Dev <shivamgoswami.jsr2025@gmail.com>
2024-01-25 21:53:26 +0530

    Added Javascript

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let input = document.getElementById("inputBox");
 let buttons = document.querySelectorAll("button");
 
 let string = "";
+
 let arr = Array.from(buttons);
 arr.forEach((button) => {
   button.addEventListener("click", (e) => {
